b6f0cb5cfa2a0fc29bfa7a8fd1ad3efecdbe8e60
wangyi85 <jackywang8911@gmail.com>
2023-09-18 21:16:42 +0800

    Modify middleware because of error while deploying to VM

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,17 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
